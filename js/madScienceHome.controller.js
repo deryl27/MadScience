86560d34c644fcbae8df3080c5041100586418f1
rahulk2014 <rahul014@ccs.neu.edu>
2016-04-09 14:48:51 -0400

    Finished Mad World BillBoard

--- js/madScienceHome.controller.js
+++ js/madScienceHome.controller.js
@@ -8,15 +8,17 @@ madscienceapp.controller('madScienceController', function($rootScope, $scope, $l
     
     //Experiment Tab should always be open on arriving in Mad World
     $scope.tabModel = "Experiment";
+    $scope.experimentContent = "Click on any Window";
+
 
     $scope.experimentWindows = [
 	
-		    {billBoardImage : "imageURL", nameOnWindow : "Exp1"},
-		    {billBoardImage : "imageURL", nameOnWindow : "Exp2"},
-		    {billBoardImage : "imageURL", nameOnWindow : "Exp3"},
-		    {billBoardImage : "imageURL", nameOnWindow : "Exp4"},
-		    {billBoardImage : "imageURL", nameOnWindow : "Exp5"},
-		    {billBoardImage : "imageURL", nameOnWindow : "Exp6"}
+		    {billBoardImage : "imageURL1", nameOnWindow : "Exp1"},
+		    {billBoardImage : "imageURL2", nameOnWindow : "Exp2"},
+		    {billBoardImage : "imageURL3", nameOnWindow : "Exp3"},
+		    {billBoardImage : "imageURL4", nameOnWindow : "Exp4"},
+		    {billBoardImage : "imageURL5", nameOnWindow : "Exp5"},
+		    {billBoardImage : "imageURL6", nameOnWindow : "Exp6"}
     ];
 
 
@@ -29,10 +31,13 @@ madscienceapp.controller('madScienceController', function($rootScope, $scope, $l
         // On Click of any window. Load the billBoard with that experiment Image.
         $scope.displayExperimentData = function(experiment) {
         	console.log("Inside experiment Tab");
+        	
         	//Go To the Experiment Tab
         	var openedTab = $scope.tabModel;
         	if (openedTab === "Filter") {
         		$scope.tabModel = "Experiment";
         	}
+        	//Display Experiment Images.
+        	$scope.experimentContent = experiment.billBoardImage;
         }
 });
